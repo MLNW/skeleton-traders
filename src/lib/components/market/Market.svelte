689ca1a03a6e1806755ffaa95f2df29be9646052
Lucas Resch <lucas.resch@gmx.de>
2023-05-18 15:25:50 +0200

    Extract market into dedicated component

---
+++ src/lib/components/market/Market.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import TradeGoodTable from '$lib/components/market/TradeGoodTable.svelte';
+  import TransactionTable from '$lib/components/market/TransactionTable.svelte';
+  import Chips from '../Chips.svelte';
+
+  export let market: object;
+</script>
+
+{#if Object.keys(market).length > 0}
+  <div class="card m-2">
+    <header class="card-header">
+      <h2>Market</h2>
+    </header>
+    <footer class="card-footer">
+      {#if market.tradeGoods}
+        <section>
+          <h3>Trade Goods</h3>
+          <TradeGoodTable data={market.tradeGoods} />
+        </section>
+        <section>
+          <h3>Transactions</h3>
+          <TransactionTable data={market.transactions} />
+        </section>
+      {:else}
+        <section>
+          <h3>Imports</h3>
+          <Chips values={market.imports} />
+        </section>
+        <section>
+          <h3>Exports</h3>
+          <Chips values={market.exports} />
+        </section>
+        <section>
+          <h3>Exchange</h3>
+          <Chips values={market.exchange} />
+        </section>
+      {/if}
+    </footer>
+  </div>
+{/if}
--- src/routes/systems/[symbol]/waypoints/[waypointSymbol]/+page.svelte
+++ src/routes/systems/[symbol]/waypoints/[waypointSymbol]/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import Chips from '$lib/components/Chips.svelte';
-  import TradeGoodTable from '$lib/components/market/TradeGoodTable.svelte';
-  import TransactionTable from '$lib/components/market/TransactionTable.svelte';
+  import Market from '$lib/components/market/Market.svelte';
 
   $: ({ symbol, systemSymbol, type, x, y, faction, traits, chart } = $page.data.waypoint.data);
 </script>
@@ -19,35 +18,5 @@
     <Chips values={traits} none={false} />
   </div>
 
-  {#if Object.keys($page.data.market.data).length > 0}
-    <div class="card m-2">
-      <header class="card-header">
-        <h2>Market</h2>
-      </header>
-      <footer class="card-footer">
-        <section>
-          <h3>Imports</h3>
-          <Chips values={$page.data.market.data.imports} />
-        </section>
-        <section>
-          <h3>Exports</h3>
-          <Chips values={$page.data.market.data.exports} />
-        </section>
-        <section>
-          <h3>Exchange</h3>
-          <Chips values={$page.data.market.data.exchange} />
-        </section>
-        {#if $page.data.market.data.tradeGoods}
-          <section>
-            <h3>Trade Goods</h3>
-            <TradeGoodTable data={$page.data.market.data.tradeGoods} />
-          </section>
-          <section>
-            <h3>Transactions</h3>
-            <TransactionTable data={$page.data.market.data.transactions} />
-          </section>
-        {/if}
-      </footer>
-    </div>
-  {/if}
+  <Market market={$page.data.market.data} />
 </div>
